4606d3276e69b627074dbf8706fc892cf61bc216
ludgerradke <radke@intervall.io>
2023-10-26 06:07:16 +0200

    MAC zip file added

--- build_mac.sh
+++ build_mac.sh
@@ -2,3 +2,5 @@
 
 # PyInstaller for onefile
 pyinstaller --onefile --windowed --collect-submodules=pydicom --icon=assets/icon.icns --add-data="assets/icon.icns:." --noconsole DicomTranslator.py --noconfirm
+
+zip -r dist/DicomTranslator_mac.zip dist/DicomTranslator.app
