c7cb942e6be46a213e09b2a980dafe93c3c14083
ludgerradke <radke@intervall.io>
2024-03-14 10:20:50 +0100

    remove files

--- build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# PyInstaller for onefile
-pyinstaller --onefile --windowed --collect-submodules=pydicom --icon=assets/icon.ico --add-data="assets/icon.ico;." --noconsole DicomTranslator.py --noconfirm
-
-# PyInstaller for onedir
-pyinstaller --onedir --windowed --collect-submodules=pydicom --icon=assets/icon.ico --add-data="assets/icon.ico;." --noconsole DicomTranslator.py --noconfirm
--- build_mac.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# PyInstaller for onefile
-pyinstaller --onefile --windowed --collect-submodules=pydicom --icon=assets/icon.icns --add-data="assets/icon.icns:." --noconsole DicomTranslator.py --noconfirm
-
-zip -r dist/DicomTranslator_mac.zip dist/DicomTranslator.app
